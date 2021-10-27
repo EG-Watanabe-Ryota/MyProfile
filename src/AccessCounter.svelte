9387e5b985e45951094d93c54bd13a89ac8a5478
Ashigaru012 <watanabe5060@gmail.com>
2021-10-27 11:22:56 +0900

    右クリック禁止！

---
+++ src/AccessCounter.svelte
@@ -0,0 +1,70 @@
+<script>
+(function () {
+    'use strict';
+
+    let mk,
+        dsSite, // サイト全体のアクセス数用
+        dsPage, // ページ単位のアクセス数用
+        now = new Date(),
+        //現在の年月日時を取ってくる
+        strToday = now.getFullYear() + "/" + ("0" + (now.getMonth() + 1)).slice(-2) + "/" + ("0" + now.getDate()).slice(-2),
+
+        site = location.hostname || "localhost",
+        page = location.pathname,
+
+        todayCountId = strToday,
+        totalCountId = "TotalCount_Page",
+        siteTotalCountId = "TotalCount_Site",
+        siteDayTotalCountId = strToday;
+
+    function accessCount(id, pageOrSite) {
+        let mk = new window.MilkCocoa("onicovyhpw.mlkcca.com"),ds;
+
+        if (pageOrSite === "page") {
+            ds = mk.dataStore(site).child(page);
+        } else if (pageOrSite === "site") {
+            ds = mk.dataStore(site);
+        }
+
+        ds.get(id, function (err, datum) {
+            if (err) {
+                // 初回はデータがなくてエラーになるので初期データをセットする
+                if (err === "not found") {
+                    ds.set(id, {
+                        "count": "1"
+                    }, function (err, datum) {
+                        mk.disconnect();
+                    });
+                } else {
+                    window.console.log(err);
+                }
+                return;
+            }
+            ds.set(id, {
+                "count": (Number(datum.value.count) + 1).toString()
+            }, function (err, datum) {
+                mk.disconnect();
+            });
+        });
+    }
+
+    /*** ページごとのアクセス数 ***/
+
+    // 日ごとのページへのアクセス数をカウント
+    accessCount(todayCountId, "page");
+
+    // トータルアクセス数をカウント
+    accessCount(totalCountId, "page");
+
+
+    /***サイト全体のカウント***/
+
+    // 日ごとのサイト全体へのアクセス数をカウント
+    accessCount(siteDayTotalCountId, "site");
+
+    // 全日のサイト全体へのアクセス数をカウント
+    accessCount(siteTotalCountId, "site");
+
+}());
+</script>
+
---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Greeting from "./Greeting.svelte";
+	import Card from "./Card.svelte";
+	import Marquee from "./Marquee.svelte";
+	import { text } from "svelte/internal";
+	document.oncontextmenu = function(){alert('右クリックは禁止です！やめてください！'); return false}
+</script>
+
+<!--本当は以下のHTML部分はindex.htmlに移した方がいいのかもしれない...-->
+<div class="main-wrapper">
+	<div class="menu">
+		<div class="img">
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<img src="ダウンロード.png">
+		</div>
+		<ul>
+			<li>トップ</li>
+			<li><a href="profile.html">プロフィール</a></li>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<li><a href="">掲示板</a></li>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<li><a href="">リンク集</a></li>
+		</ul>
+	</div>
+
+	<div class="top">
+		<div class="title">
+			<h1>ようこそ渡邊良太のホームページへ！</h1>
+			<h2>Welcome to Watanabe's HP!</h2>
+		</div>
+			<div class="content">
+				<Marquee text="Welcome to My HomePage!!" size="60" color="red"/>
+				<p class="yukkuri">ゆっくりしていってね</p>
+				<p class="jpn">Sorry,This HomePage is Japanese only!</p>
+				<p class="cnt">あなたは<span>365</span>人目の来訪者です。キリ番ゲットした方は<a href="">掲示板</a>で報告してください。キリ番踏み逃げ禁止！</p>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<p><a href="">★★相互リンク募集中です。無断リンクは禁止です★★</a></p>
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<img src="neko.png" height="200px" width="200px">
+				<p>↑我が家のネコちゃんです</p>
+			</div>
+		</div>
+	</div>
+<style>
+	  
+</style>
